9ae83a6c12ff27140915550fefbb90a54b63ad28
Kevin Matz <kevin@winchelseasystems.com>
2023-10-26 12:07:46 -0700

    Adding command to force-quit and remove existing container

--- StockChartFlaskApp/flask_app/build_and_run_docker_container.bat
+++ StockChartFlaskApp/flask_app/build_and_run_docker_container.bat
@@ -1,8 +1,10 @@
 set app=docker.stockchartapptest
+REM Remove the existing container; -f forces it to quit if it is running 
+docker rm -f %app%
 REM Build the docker container
 docker build -t %app% .
-REM Bind port 56733 to the container's port 80
+REM Bind port 57732 to the container's port 80
 REM Link the present directory to the /var/www directory of the container
 REM (-v specifies a Docker volume to mount on the container; the host source is before the colon
 REM and the destination source is after the colon)
-docker run -d -p 56733:80 --name=%app% -v .:/app %app%
+docker run -d -p 57732:80 --name=%app% -v .:/app %app%
